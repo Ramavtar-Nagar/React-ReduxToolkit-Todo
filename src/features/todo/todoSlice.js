f2f9b4b989d3daad2d4cfdfd74fca4b5a3b7538d
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-04 11:55:29 +0530

    Setting up Slice

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [{id: 1, text: "Hello My TOdo"}]
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+        }
+    }
+})
