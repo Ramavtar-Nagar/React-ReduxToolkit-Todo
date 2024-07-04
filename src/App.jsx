c9147338578a9b3a35630ef2ceea62abdad2f562
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-04 15:33:28 +0530

    Completed project including debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
-      <h1>React Redux Toolkit...</h1>
+      <h1>My Todos...</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
