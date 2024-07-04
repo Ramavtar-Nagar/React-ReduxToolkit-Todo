21f276915f87d1f0b2e19475782b6ba7cac24c0c
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-04 12:35:46 +0530

    store and slice setup configured

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -13,6 +13,14 @@ export const todoSlice = createSlice({
                 id: nanoid(),
                 text: action.payload
             }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         }
     }
-})
+}) 
+
+export const { addTodo, removeTodo} = todoSlice.actions
+
+export default todoSlice.reducer
